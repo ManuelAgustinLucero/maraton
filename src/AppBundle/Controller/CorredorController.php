c03686694ffd1db6fa8e1f838371f329e8c8e248
Natanael <ncarinao@gmail.com>
2017-12-20 17:32:33 -0300

    efectivo y numero de kit

--- src/AppBundle/Controller/CorredorController.php
+++ src/AppBundle/Controller/CorredorController.php
@@ -46,7 +46,7 @@ class CorredorController extends Controller
         $em = $this->getDoctrine()->getManager();
 
         $form = $this->createForm('AppBundle\Form\CorredorType', $corredor);
-        $form->get('formapago')->setData($em->getReference('AppBundle:FormaPago', 3));
+        $form->get('formapago')->setData($em->getReference('AppBundle:FormaPago', 2));
         $form->get('tipocarrera')->setData($em->getReference('AppBundle:TipoCarrera', 1));
 
         $form->handleRequest($request);
